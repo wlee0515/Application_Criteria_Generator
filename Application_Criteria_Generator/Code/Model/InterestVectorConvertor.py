b4919de70724f072bbe97932568ff30719fdcd46
wlee0515 <wlee0515@users.noreply.github.com>
2019-03-19 21:44:54 -0400

    Optimise interest vector converter

--- Application_Criteria_Generator/Code/Model/InterestVectorConvertor.py
+++ Application_Criteria_Generator/Code/Model/InterestVectorConvertor.py
@@ -4,18 +4,16 @@ import numpy as np
 class InterestVectorConvertor():
     def __init__(self, df_Category):       
         self.df_Category = df_Category.copy()
-        self.df_Category = self.df_Category.drop("label_id",axis=1)
+        self.list_exclude = ["label_id", "category", "category-mod"]
+        self.list_VectorColumn = [ x for x in self.df_Category.columns if x not in self.list_exclude]
 
     def convertVector(self, interestVectorPrediction):
-        wList_exclude = ["label_id", "category", "category-mod"]
-        wVectorColumn = [ x for x in self.df_Category.columns if x not in wList_exclude]
-        wdf = self.df_Category.copy()
-        wInterestVector = pd.DataFrame(interestVectorPrediction, columns=wVectorColumn)
-        for word in wVectorColumn:
-            wdf[word] = wdf[word].apply(lambda x : x*wInterestVector[word])
-        wdf["category-score"] = wdf.sum(axis=1);
-        wdf = wdf[["category-mod","category-score"]]
-        wdf = wdf.groupby("category-mod").mean().reset_index()
+        wWorkingMatrix = self.df_Category.drop(self.list_exclude, axis=1).to_numpy()
+        wScore = np.matmul(wWorkingMatrix, np.transpose(interestVectorPrediction))
+        wdf = self.df_Category["category-mod"].to_frame();
+        wdf_score = pd.DataFrame(wScore, columns =["category-score"])
+        wdf = pd.concat([wdf, wdf_score], axis=1)
+        wdf = wdf.groupby("category-mod").mean()
         wdf = wdf.sort_values("category-score", ascending=False).head(25)
         return wdf
         
